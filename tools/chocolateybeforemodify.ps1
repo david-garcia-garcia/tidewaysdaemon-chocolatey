9dd56298b6a53880fca07fbff8bac3df6151a382
David Garcia <deivid.garcia.garcia@gmail.com>
2017-07-29 10:43:49 +0200

    Initial commit.

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,2 @@
+﻿# Make sure we stop the service, to unlock the daemon binary.
+nssm stop TidewaysDaemon
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= 'tidewaysdaemon' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'daemon_windows_amd64.exe'
+
+Netsh.exe advfirewall firewall add rule name="Tideways Daemon" program=$fileLocation protocol=tcp dir=out enable=yes action=allow
+
+$destinationPath = $fileLocation
+$destinationDir = $toolsDir
+
+if (!(Get-Service -Name "TidewaysDaemon" -ErrorAction SilentlyContinue))
+{
+    nssm install TidewaysDaemon $destinationPath
+}
+
+
+nssm set TidewaysDaemon Application  $destinationPath
+nssm set TidewaysDaemon AppDirectory $destinationDir
+nssm set TidewaysDaemon AppParameters '--address="127.0.0.1:8136"'
+
+nssm set TidewaysDaemon DisplayName Tideways Daemon
+nssm set TidewaysDaemon Description The Tideways Profiler Daemon
+nssm set TidewaysDaemon Start SERVICE_AUTO_START
+
+nssm start TidewaysDaemon
