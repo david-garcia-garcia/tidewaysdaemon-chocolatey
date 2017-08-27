bdea35d51beb4bd0a29c4ca0b2e8a6826e91d3f7
David Garcia <deivid.garcia.garcia@gmail.com>
2017-08-27 17:58:45 +0200

    Fix reviewer issues.

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -1,2 +1,1 @@
-﻿# Make sure we stop the service, to unlock the daemon binary.
-nssm stop TidewaysDaemon
+﻿nssm stop TidewaysDaemon
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,6 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName= 'tidewaysdaemon' # arbitrary name for the package, used in messages
+$packageName= 'tidewaysdaemon'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'daemon_windows_amd64.exe'
 
